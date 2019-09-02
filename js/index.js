88572764c698e7df7c92592a769d10e5a0dd4aea
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-02 13:58:29 +0100

    update the navigation

--- js/index.js
+++ js/index.js
@@ -40,3 +40,19 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//Update the navigation
+let topNav = document.querySelectorAll("a");
+topNav[0].textContent = siteContent.nav["nav-item-1"]
+topNav[1].textContent = siteContent.nav["nav-item-2"]
+topNav[2].textContent = siteContent.nav["nav-item-3"]
+topNav[3].textContent = siteContent.nav["nav-item-4"]
+topNav[4].textContent = siteContent.nav["nav-item-5"]
+topNav[5].textContent = siteContent.nav["nav-item-6"]
+
+//Update the ct
+let ctaElement = document.getElementsByClassName("cta-text")[0].children;
+ctaElement[0].textContent = siteContent.cta["h1"]
+ctaElement[1].textContent = siteContent.cta["button"]
+let ctaLogo = document.getElementById("cta-img");
+ctaLogo.setAttribute('src', siteContent.cta["img-src"])
